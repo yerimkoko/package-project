ad1f119147b7e6ef21a1d3c4fd6468b3e58d6439
yerimkoko <gochi97@naver.com>
2024-06-18 15:22:36 +0900

    안쓰는 코드 정리

--- src/main/java/com/example/packageproject/service/request/PackagesRequest.java
+++ src/main/java/com/example/packageproject/service/request/PackagesRequest.java
@@ -2,9 +2,7 @@ package com.example.packageproject.service.request;
 
 import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Positive;
 import lombok.AccessLevel;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -16,7 +14,6 @@ public class PackagesRequest {
     private Long cursor;
 
     @Max(value = 30)
-    @Positive
     private int size;
 
     private PackagesRequest(Long cursor, int size) {
